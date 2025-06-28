dcd0654e8e5f93902ac35d56f7c86032379b6ac7
Avnish Singh <avnish1708@gmail.com>
2025-06-28 16:28:25 +0530

    config and model loader defined

---
+++ README.md
@@ -0,0 +1,14 @@
+uv venv -p python3.10.18
+.venv/Scripts/activate
+
+uv add -r requirements.txt
+or
+uv pip install -r requirements.txt
+or 
+uv pip install .[dev]  # you must have the pyproject.toml with all the dependencies [ for replicability]
+
+# you can also do this if you have the pyproejct.toml
+uv pip compile > requirements.txt
+uv pip install -r requirements.txt
+
+
---
+++ agent/agent_workflow.py
@@ -0,0 +1,24 @@
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+
+    def agent_fucntion(self):
+        pass
+
+    
+    def build_graph(self):
+        pass
+    
+
+    def __call__(self):
+        return self.build_graph()
