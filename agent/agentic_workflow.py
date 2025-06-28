c17a76c76470c04675847fd163f8ecc802fb8e27
Avnish Singh <avnish1708@gmail.com>
2025-06-28 17:30:41 +0530

    weather tool added

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,21 +1,34 @@
 from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
 
-# from tools.weather_info_tool import WeatherInfoTool
-# from tools.place_search_tool import PlaceSearchTool
-# from tools.expense_calculator_tool import CalculatorTool
-# from tools.currency_conversion_tool import CurrencyConverterTool
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
 from utils.model_loader import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
 
 class GraphBuilder():
-    def __init__(self):
-        self.tools = [
+    def __init__(self, model_provider:str = "groq"):
+        self.tools = []
+        self.model_laoder = ModelLoader(model_provider = model_provider)
+        self.llm = self.model_loader.load_llm()
 
-        ]
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
 
+        self.tools.extend([
+            * self.weather_tools.weather_tool_list,
+            * self.place_search_tools.place_search_tool_list,
+            * self.calculator_tools.calculator_tool_list,
+            * self.currency_converter_tools.currency_converter_tool_list
+        ])
+
+        self.graph = None
         self.system_prompt = SYSTEM_PROMPT
-        self.llm_with_tools = ModelLoader().load_llm()
+        self.llm_with_tools = self.llm_bind_tools(tools=self.tools)
 
     def agent_fucntion(self,state: MessagesState):
         "Main agent function"
@@ -36,6 +49,5 @@ class GraphBuilder():
         self.graph = graph_builder.compile()
         return self.graph
     
-
     def __call__(self):
         return self.build_graph()
